cc50f4b4013138290acd525fafcb589a89b68d6d
TzuShianYang <c4940578@gmail.com>
2019-05-24 01:27:27 +0800

    Add test file

--- code11.c
+++ code11.c
@@ -1,5 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
 
 int main(){
-	
+
+    File *filePtr;
+
+    if((filePtr=fopen("test1.txt","r"))==NULL){
+        puts("0");
+    }
+    else{
+        puts("1");
+    }
+
 }
