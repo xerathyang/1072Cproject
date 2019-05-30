37dfd209768518fa4c8cb0ec3351303290d23036
TzuShianYang <c4940578@gmail.com>
2019-05-30 17:32:03 +0800

    upload new file

---
+++ code11.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+void badcode(){
+    puts("bad code");
+}
+
+
+int main(){
+
+    FILE *filePtr;
+    int codeLength;
+    int smallest =150;
+    int reverse;
+
+    //check file and get length
+    if((filePtr=fopen("test1.txt","r"))==NULL){
+        puts("Couldn't find file.");
+        exit(0);
+    }else{
+        fscanf(filePtr,"%d",&codeLength);
+    }
+    int codeContent[codeLength];
+
+    //get content
+    for(int i=0; i <codeLength;i++){
+        fscanf(filePtr,"%d",&codeContent[i]);
+    }
+
+    //find smallest and convert content to binary
+    for(int x=0; x <codeLength;x++){
+        if(codeContent[x]<smallest)
+            smallest= codeContent[x];
+        codeContent[x]=(int)round((float)codeContent[x]/smallest)-1;
+    }
+
+    //check it whether is reverse or not
+    if(codeContent[0]==0&&codeContent[1]==0&&codeContent[2]==1&&codeContent[3]==1&&codeContent[4]==0&&codeContent[codeLength-1]==0&&codeContent[codeLength-2]==1&&codeContent[codeLength-3]==1&&codeContent[codeLength-4]==0&&codeContent[codeLength-5]==0){
+        reverse=0;
+    }else if(codeContent[0]==0&&codeContent[1]==1&&codeContent[2]==1&&codeContent[3]==0&&codeContent[4]==0&&codeContent[codeLength-1]==0&&codeContent[codeLength-2]==0&&codeContent[codeLength-3]==1&&codeContent[codeLength-4]==1&&codeContent[codeLength-5]==0){
+        reverse=1;
+    }else{
+        badcode();
+    }
+
+    for(int u=0; u <codeLength;u++){
+        printf("%d ",codeContent[u]);
+        if(u%10 ==0)
+            puts("");
+    }
+}
