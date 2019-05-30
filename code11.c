10427850c6023bb58cf95fa9087cdf866f1a513f
xerathyang <c4940578@gmail.com>
2019-05-31 00:51:29 +0800

    add test end tag, a lot of bug need to fix

--- code11.c
+++ code11.c
@@ -2,26 +2,25 @@
 #include <math.h>
 #include <stdlib.h>
 
-int 
-
 int main(){
 
     FILE *filePtr;
     unsigned int codeLength=1;
     int smallest =150;
-    int reverse,encodeLength;
+    int reverse,encodeLength,hold,count;
 
     //check file and get length
-    if((filePtr=fopen("test1.txt","r"))==NULL){
-        puts("Couldn't find file.");
+    if((filePtr=fopen("test2.txt","r"))==NULL){
+        puts("Couldnt find file.");
         exit(0);
     }
 	
+	fscanf(filePtr,"%d",&codeLength);
+	
 	while(codeLength!=0){
-		fscanf(filePtr,"%d",&codeLength);
-    
+		
 		int codeContent[codeLength];
-		encodeLength= (codeLength-23)/6
+		encodeLength= (codeLength-23)/6;
 		
 		//get content
 		for(int i=0; i <codeLength;i++){
@@ -34,60 +33,70 @@ int main(){
 				smallest= codeContent[x];
         codeContent[x]=(int)round((float)codeContent[x]/smallest)-1;
 		}
-
+		
+		for(int u=0; u <codeLength;u++){
+			printf("%d ",codeContent[u]);
+			count++;
+			if(count%10 ==0)
+				puts("");
+		}
+		
 		//check it whether is reverse or not
 		if(codeContent[0]==0&&codeContent[1]==0&&codeContent[2]==1&&codeContent[3]==1&&codeContent[4]==0&&codeContent[codeLength-1]==0&&codeContent[codeLength-2]==1&&codeContent[codeLength-3]==1&&codeContent[codeLength-4]==0&&codeContent[codeLength-5]==0){
 			reverse=0;
 		}else if(codeContent[0]==0&&codeContent[1]==1&&codeContent[2]==1&&codeContent[3]==0&&codeContent[4]==0&&codeContent[codeLength-1]==0&&codeContent[codeLength-2]==0&&codeContent[codeLength-3]==1&&codeContent[codeLength-4]==1&&codeContent[codeLength-5]==0){
 			reverse=1;
+			puts("reverse");
+			for(int z=0;z<=codeLength/2;z++){
+				hold=codeContent[z];
+				codeContent[z]=codeContent[codeLength-z-1];
+				codeContent[codeLength-z-1]=hold;
+			}
 		}else{
 			puts("bad code(start code lost)");
-			continue;
+			system("pause");
+		}
+		puts("");
+		
+		for(int y=1; y<=encodeLength+2;y++){
+			if(codeContent[5*y+2]==1){
+				if(codeContent[5*y+3]==1){
+					printf("5");
+				}else if(codeContent[5*y+4]==1){
+					printf("3");
+				}else if(codeContent[5*y+5]==1){
+					printf("1");
+				}else if(codeContent[5*y+6]==1){
+					printf("8");
+				}else{
+					printf("0");
+				}
+			}else{
+				if(codeContent[5*y+3]==0){
+					if(codeContent[5*y+4]==0){
+						if(codeContent[5*y+5]==0){
+							printf("9");
+						}else{
+							printf("2");
+						}
+					}else if(codeContent[5*y+5]==1){
+						printf("S");
+					}else if(codeContent[5*y+6]==1){
+						printf("6");
+					}else{
+						printf("-");
+					}
+				}else if(codeContent[5*y+4]==1){
+					printf("4");
+				}else{
+					printf("7");
+				}
+			}
+				
 		}
 		
-		if(reverse=0){
-			for(int y=1; y<=encodeLength+2;y++){
-				if(codeContent[5y+2]==1){
-					if(codeContent[5y+3]==1){
-						printf('5');
-					}else if(codeContent[5y+4]==1){
-						printf('3');
-					}else if(codeContent[5y+5]==1){
-						printf('1');
-					}else if(codeContent[5y+6]==1){
-						printf('8');
-					}else{
-						printf('0');
-					}
-				}else{
-					if(codeContent[5y+3]==0){
-						if(codeContent[5y+4]==0){
-							if(codeContent[5y+5]==0){
-								printf('9');
-							}else{
-								printf('2');
-							}
-						}else if(codeContent[5y+5]==1){
-							printf('S');
-						}else if(codeContent[5y+6]==1){
-							printf('6');
-						}else{
-							printf('-');
-						}
-					}else if(codeContent[5y+4]==1){
-						printf('4');
-					}else{
-						printf('7');
-					}
-				}
-				
-			}
-		}
-
-		//for(int u=0; u <codeLength;u++){
-		//	printf("%d ",codeContent[u]);
-		//	if(u%10 ==0)
-		//		puts("");
-		//}
+		fscanf(filePtr,"%d",&codeLength);
+		
 	}
+	system("pause");
 }
