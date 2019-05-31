2a18f5bb094cd823622a60a22276815b875b0ac6
TzuShianYang <c4940578@gmail.com>
2019-05-31 13:58:13 +0800

    need to fix something

--- code11.c
+++ code11.c
@@ -7,7 +7,7 @@ int main(){
     FILE *filePtr;
     unsigned int codeLength=1;
     int smallest =150;
-    int reverse,encodeLength,hold;
+    int reverse,encodeLength,hold,c,k;
 	int realc,realk=0;
 
     //check file and get length
@@ -73,33 +73,33 @@ int main(){
 		printf("testpoint");
 		puts("");
 		
-		for(int y=1; y<encodeLength-2;y++){
+		for(int y=0; y<encodeLength;y++){
 			if(codeContent[6*y]==1){//1XXXX
 				if(codeContent[6*y+1]==1){//11000
 					//printf("5");
-					encodeContent[y-1]=5;
+					encodeContent[y]=5;
 				}else if(codeContent[6*y+2]==1){//10100
 					//printf("3");
-					encodeContent[y-1]=3;
+					encodeContent[y]=3;
 				}else if(codeContent[6*y+3]==1){//10010
 					//printf("1");
-					encodeContent[y-1]=1;
+					encodeContent[y]=1;
 				}else if(codeContent[6*y+4]==1){//10001
 					//printf("8");
-					encodeContent[y-1]=8;
+					encodeContent[y]=8;
 				}else{//10000
 					//printf("0");
-					encodeContent[y-1]=0;
+					encodeContent[y]=0;
 				}
 			}else{//0XXXX
 				if(codeContent[6*y+1]==0){//00XXX
 					if(codeContent[6*y+2]==0){//000XX
 						if(codeContent[6*y+3]==0){//0000X
 							//printf("9");
-							encodeContent[y-1]=9;
+							encodeContent[y]=9;
 						}else{//0001X
 							//printf("2");
-							encodeContent[y-1]=2;
+							encodeContent[y]=2;
 						}
 					}else{//001XX
 						if(codeContent[6*y+3]==1){//0011X
@@ -107,110 +107,24 @@ int main(){
 							exit(0);
 						}else if(codeContent[6*y+4]==1){//00
 							//printf("6");
-							encodeContent[y-1]=6;
+							encodeContent[y]=6;
 						}else{
 							//printf("-");
-							encodeContent[y-1]=10;
+							encodeContent[y]=10;
 						}
 					}
 				}else if(codeContent[6*y+2]==1){
 					//printf("4");
-					encodeContent[y-1]=4;
+					encodeContent[y]=4;
 				}else{
 					//printf("7");
-					encodeContent[y-1]=7;
+					encodeContent[y]=7;
 				}
 			}
 		}
-		for(int w=encodeLength-1; w<=encodeLength; w++){
-			if(codeContent[6*w]==1){//1XXXX
-				if(codeContent[6*w+1]==1){//11000
-					if(w==encodeLength){
-						encodeContent[w-1]=5;
-					}else{
-						encodeContent[w-2]=5;
-					}
-				}else if(codeContent[6*w+2]==1){//10100
-					if(w==encodeLength){
-						encodeContent[w-1]=3;
-					}else{
-						encodeContent[w-2]=3;
-					}
-				}else if(codeContent[6*w+3]==1){//10010
-					if(w==encodeLength){
-						encodeContent[w-1]=1;
-					}else{
-						encodeContent[w-2]=1;
-					}
-				}else if(codeContent[6*w+4]==1){//10001
-					if(w==encodeLength){
-						encodeContent[w-1]=8;
-					}else{
-						encodeContent[w-2]=8;
-					}
-				}else{//10000
-					if(w==encodeLength){
-						encodeContent[w-1]=0;
-					}else{
-						encodeContent[w-2]=0;
-					}
-				}
-			}else{//0XXXX
-				if(codeContent[6*w+1]==0){//00XXX
-					if(codeContent[6*w+2]==0){//000XX
-						if(codeContent[6*w+3]==0){//0000X
-							if(w==encodeLength){
-								encodeContent[w-1]=9;
-							}else{
-								encodeContent[w-2]=9;
-							}
-						}else{//0001X
-							if(w==encodeLength){
-								encodeContent[w-1]=2;
-							}else{
-								encodeContent[w-2]=2;
-							}
-						}
-					}else{//001XX
-						if(codeContent[6*w+3]==1){//0011X
-							printf("bad code(start code wrong place)");
-							exit(0);
-						}else if(codeContent[6*w+4]==1){//00
-							if(w==encodeLength){
-								encodeContent[w-1]=6;
-							}else{
-								encodeContent[w-2]=6;
-							}
-						}else{
-							if(w==encodeLength){
-								encodeContent[w-1]=10;
-							}else{
-								encodeContent[w-2]=10;
-							}
-						}
-					}
-				}else if(codeContent[6*w+2]==1){
-					if(w==encodeLength){
-						encodeContent[w-1]=4;
-					}else{
-						encodeContent[w-2]=4;
-					}
-				}else{
-					if(w==encodeLength){
-						encodeContent[w-1]=7;
-					}else{
-						encodeContent[w-2]=7;
-					}
-				}
-			}
-		}
+		
 		
 		puts("");
-		
-		for(int q=1; q<=encodeLength; q++){
-			realc+=((encodeLength-q)%10+1)*encodeContent[q-1];
-			realc%=11;
-		}
 		
 		for(int r=0; r<encodeLength-2; r++){
 			if(encodeContent[r]!=10){
@@ -219,6 +133,22 @@ int main(){
 				printf("-");
 			}
 		}
+		
+		puts("");
+		
+		//C Check
+		realc=0;
+		printf("%d",encodeLength);
+		printf("%d",encodeContent[encodeLength-3]);
+		for(int i=1; i<=encodeLength-2; i++){
+			realc+=((encodeLength-2-i)%10+1)*encodeContent[i-1];
+		}
+		realc=realc%11;
+		if(realc!=encodeContent[encodeLength-3])
+			system("pause");
+		
+		
+		
 		printf("c=%d",realc);
 		fscanf(filePtr,"%d",&codeLength);
 		
